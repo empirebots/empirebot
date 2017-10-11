022543ca9caceb87ebfef4bb5169303ddcad1ebf
Simon Vandeputte <simon.vandeputte@etudiant.univ-lille1.fr>
2017-10-11 22:46:50 +0200

    feat:
    A bit of refactoring and a new command 'man' that can be used to learn about a specific command.

--- index.js
+++ index.js
@@ -4,57 +4,71 @@ const client = new Discord.Client();
 
 let PREFIX = '!!';
 
-client.on('ready', function () {
+
+client.on('ready', function ()
+{
     console.log('Logged in as $(client.user.tag)');
 });
 
 
-client.on('message', msg => {
-    if (msg.channel.type === 'text' && !msg.author.bot && msg.content.startsWith(PREFIX)) {
-        msg.channel.send('Hell Yeah !').then(msg => {
-            msg.react('👍').catch();
-        }).catch();
-    }
-
-    if (msg.content.startsWith(PREFIX + 'help')) {
+client.on('message', msg =>
+{
+    if (msg.content.startsWith(PREFIX + 'help'))
+    {
         let RichEmbed = new Discord.RichEmbed();
         RichEmbed.setColor("#63dded");
         RichEmbed.setTitle('Help');
         RichEmbed.setDescription('Here are few useful things that I should be able to do, in order to help you :smirk:');
-        fs.readdir('resources/man', 'utf8', (err, files) => {
-            files.forEach((element) => {
+        fs.readdir('resources/man', 'utf8', (err, files) =>
+        {
+            files.forEach((element) =>
+            {
                 let strFinal;
                 console.log(element);
                 let data = fs.readFileSync('resources/man/' + element, 'utf8');
                 let tabData = data.split('\n');
                 let flagBoolean = false, flag = false;
                 console.log(tabData.length);
-                tabData.forEach((data) => {
-
-                    console.log(data);
-
-                    if (flag) {
+                tabData.forEach((data) =>
+                {
+                    if (flag)
+                    {
                         strFinal = data;
                         flag = false;
                         console.log(strFinal);
                     }
-                    if (flagBoolean) {
+                    if (flagBoolean)
+                    {
                         flag = true;
                         flagBoolean = false;
                     }
 
-                    if (data.includes('Synopsis')) {
-                        console.log(data);
+                    if (data.includes('Synopsis'))
+                    {
                         flagBoolean = true;
                     }
                 });
-
                 if (strFinal !== undefined)
                     RichEmbed.addField(element.substring(0, element.length - 3), strFinal, true);
             });
-
             msg.author.send(RichEmbed);
         });
+    }
+
+    if (msg.content.startsWith(PREFIX + 'man'))
+    {
+        let tab = msg.content.split(' ');
+        if (tab.length <= 1)
+        {
+            msg.channel.send("```bash\nMissingArgumentException\n```").catch();
+        } else
+        {
+            let strCommand = tab[1];
+            strCommand += '.md';
+            let data = fs.readFileSync('resources/man/' + strCommand, 'utf8');
+            msg.author.send(data);
+            console.log(strCommand);
+        }
     }
 
 });
